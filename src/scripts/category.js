1102aa04cf5ef2b7d2b88eb8d20c053a49a6e110
Abraham <afontana@unedbarbastro.es>
2024-04-26 10:54:28 +0200

    Add category page and script to display album links

---
+++ src/scripts/category.js
@@ -0,0 +1,17 @@
+import { albums } from "../data/albumsData.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("album-links");
+  albums.forEach((album) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+      <img loading="lazy" src="${album.image}" alt="${album.title}" />
+      <div class="card-body">
+        <h3>${album.title}</h3>
+        <p>${album.shortDescription}</p>
+        <a href="detail.html?id=${album.id}" class="btn">Más Info</a>
+      </div>`;
+    container.appendChild(card);
+  });
+});
