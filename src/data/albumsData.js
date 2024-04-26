b97344ad7719fe09cd6dff37ab67b92da8d474f9
Abraham <afontana@unedbarbastro.es>
2024-04-26 10:52:45 +0200

    add json with data

---
+++ src/data/albumsData.js
@@ -0,0 +1,229 @@
+import laPateraImage from "../assets/images/albums/01-la-patera-670x670.jpg";
+import revolconImage from "../assets/images/albums/02-revolcon-670x670.jpg";
+import punaladasImage from "../assets/images/albums/03-28000-punaladas-670x670.jpg";
+import besosDePerroImage from "../assets/images/albums/04-besos-de-perro-670x670.jpg";
+import acerasImage from "../assets/images/albums/05-las-aceras-estan-llenas-de-piojos-1-670x670.jpg";
+import jauriaImage from "../assets/images/albums/06-jauria-de-perros-verdes-670x670.jpg";
+import viejasImage from "../assets/images/albums/07-las-putas-mas-viejas-del-mundo-670x670.jpg";
+import hambreImage from "../assets/images/albums/08-en-mi-hambre-mando-yo-670x670.jpg";
+import azogueImage from "../assets/images/albums/09-el-azogue-670x670.jpg";
+import potrosImage from "../assets/images/albums/10-los-potros-del-tiempo-670x670.jpg";
+
+export const albums = [
+  {
+    id: 1,
+    title: "La Patera",
+    image: laPateraImage,
+    shortDescription:
+      "El álbum debut que marcó un antes y un después en la escena del rock español.",
+    longDescription:
+      "La Patera es el primer álbum de estudio de Marea, lanzado en el año 1999. Representa la primera incursión seria de la banda en el mundo de la música, ofreciendo una mezcla robusta de rock y letras poéticas que abordan temas sociales y personales. Este álbum estableció a Marea como una de las bandas más prometedoras del rock español, con un estilo que combina la crudeza del rock con una sensibilidad poética.\nDesde el lanzamiento de La Patera, Marea ha continuado evolucionando su sonido, pero este álbum sigue siendo emblemático por su frescura y energía. Las canciones de este álbum, como 'Barniz' y 'Marea', se han convertido en himnos para sus seguidores y han establecido la base de lo que sería el sonido característico de la banda.\nLa recepción del álbum fue positivamente sólida, y aunque fue solo el comienzo, marcó un hito en la carrera de la banda. Los seguidores aprecian este álbum por su autenticidad y la pasión que Marea transmite a través de sus letras y melodías.",
+    songs: [
+      "Marea",
+      "Ya lo dijo Camarón",
+      "Trasegando",
+      "No quiero ser un poeta",
+      "Despellejo",
+      "Quejíos",
+      "Lija y terciopelo",
+      "Como quiere tu abuelita",
+    ],
+  },
+  {
+    id: 2,
+    title: "Revolcón",
+    image: revolconImage,
+    shortDescription:
+      "El segundo álbum de Marea, con riffs más duros y letras comprometidas.",
+    longDescription:
+      "Publicado en el año 2000, Revolcón es el segundo álbum de estudio de Marea y sigue fortaleciendo la reputación de la banda dentro de la escena del rock español. Este álbum es conocido por sus guitarras más agresivas y una actitud más confrontativa en sus letras, reflejando la madurez y la evolución del grupo.\nCon temas que exploran desde la crítica social hasta historias de vida cotidiana, Revolcón se destaca por canciones como 'Que se joda el viento', una de las más queridas por los fans de la banda. Este álbum consolidó a Marea como una de las voces más distintivas y críticas del rock en español, utilizando su música como un vehículo para comentar sobre la injusticia y la desigualdad.\nLa producción de este álbum también mostró un avance significativo respecto a su predecesor, con arreglos más complejos y una calidad de sonido mejorada. La banda ganó más seguidores y Revolcón se considera un clásico en su discografía.",
+    songs: [
+      "Barniz",
+      "Incandescente",
+      "Amor temporero",
+      "Corazón de mimbre",
+      "El perro verde",
+      "Duerme conmigo",
+      "Canto de tierra seca I",
+      "Canto de tierra seca II",
+      "Prima tristeza",
+      "Mojama",
+      "Si viene la pestañí",
+    ],
+  },
+  {
+    id: 3,
+    title: "28.000 Puñaladas",
+    image: punaladasImage,
+    shortDescription:
+      "Una intensa exploración de emociones en el tercer álbum de Marea.",
+    longDescription:
+      "28.000 Puñaladas, lanzado en 2004, lleva el nombre de la intensidad emocional y lírica que Marea inyecta en cada pista. Este álbum se caracteriza por su enfoque ligeramente más melódico, aunque sin perder la fuerza del rock que define a la banda. La combinación de letras poéticas y poderosas melodías crea una experiencia auditiva que es tanto conmovedora como energizante.\nLas canciones abordan temas de amor, pérdida y resistencia, reflejando las luchas y la resiliencia del espíritu humano. Hits como 'La luna me sabe a poco' se han convertido en favoritos entre los fanáticos, demostrando la habilidad de la banda para conectar con su audiencia a un nivel profundo.\nEste álbum no solo consolidó la fama de Marea en España sino que también les ganó reconocimiento internacional. La capacidad de la banda para fusionar la poesía con el rock en una forma que resuena universalmente es evidente a lo largo de este poderoso álbum.",
+    songs: [
+      "La rueca",
+      "A caballo",
+      "Virgen del fracaso",
+      "Con la camisa rota",
+      "Latido jondo",
+      "El hijo de la Inés",
+      "Dos alpargatas",
+      "Como los trileros",
+      "Ciudad de los gitanos",
+      "Al culo de una lombriz",
+      "Que se joda el viento",
+    ],
+  },
+  {
+    id: 4,
+    title: "Besos de Perro",
+    image: besosDePerroImage,
+    shortDescription:
+      "Un álbum que explora nuevos horizontes musicales manteniendo la esencia de Marea.",
+    longDescription:
+      "Lanzado en 2002, 'Besos de Perro' es una exploración profunda y personal de los temas y sonidos que han definido a Marea. Con una producción más elaborada, el álbum ofrece una variedad de estilos musicales, desde baladas melódicas hasta rock potente, mostrando la versatilidad de la banda.\nEste álbum contiene algunos de los éxitos más memorables de Marea, como 'La luna me sabe a poco', que se ha convertido en una canción icónica en su repertorio. 'Besos de Perro' también es notable por su riqueza lírica, con letras que profundizan en las emociones humanas y las complejidades de la vida cotidiana.\nCon 'Besos de Perro', Marea no solo consolidó su posición en el rock español, sino que también expandió su audiencia internacionalmente. Este álbum es celebrado por su capacidad para conectar emocionalmente con los oyentes a través de su poesía y su música apasionada.",
+    songs: [
+      "Romance de Jose Etxailarena",
+      "El rastro",
+      "La luna me sabe a poco",
+      "A la mierda primavera",
+      "En tu agujero",
+      "Manuela canta saetas",
+      "Alfileres",
+      "Me estoy quedando solo",
+      "Venas con humo y palabras",
+      "Como el viento de poniente",
+      "Pan duro",
+    ],
+  },
+  {
+    id: 5,
+    title: "Las Aceras Están Llenas de Piojos",
+    image: acerasImage,
+    shortDescription:
+      "Un álbum crítico que refleja la realidad social con la poesía única de Marea.",
+    longDescription:
+      "Publicado en 2007, 'Las Aceras Están Llenas de Piojos' es un testimonio del estilo característico de Marea, combinando rock duro con lírica poética. Este álbum se adentra en temas sociales críticos con un enfoque directo y sin concesiones.\nLas canciones de este álbum, incluyendo 'Ciudad de los Gitanos', ofrecen un comentario agudo sobre la injusticia y la marginación, mientras que también celebran la resistencia y la esperanza. La capacidad de la banda para narrar historias a través de su música se manifiesta poderosamente en este álbum.\n'Las Aceras Están Llenas de Piojos' es apreciado tanto por la crítica como por los fanáticos por su compromiso con la autenticidad y su enfoque sin miedo a abordar temas difíciles, asegurando su lugar como un trabajo significativo en la discografía de Marea.",
+    songs: [
+      "Entre hormigones",
+      "Aceitunero",
+      "Por cuatro perras",
+      "El trapecio",
+      "Mierda y cuchara",
+      "Mil quilates",
+      "Petenera (en carne viva)",
+      "Nana de quebranto (mala sombra)",
+      "Me corten la lengua",
+      "La hora de las moscas",
+      "Los mismos clavos",
+    ],
+  },
+  {
+    id: 6,
+    title: "La Jauría de Perros Verdes",
+    image: jauriaImage,
+    shortDescription:
+      "Marea regresa a sus raíces con un álbum lleno de energía y crítica social.",
+    longDescription:
+      "En 'La Jauría de Perros Verdes', lanzado en 2011, Marea vuelve a su sonido clásico de rock enérgico y letras cargadas de crítica social. Este álbum se caracteriza por su enfoque directo y su estilo musical robusto, con guitarras potentes y una percusión contundente.\nEl álbum abre con 'Bienvenido al Secadero', una poderosa declaración de las intenciones de la banda de enfrentar las injusticias sociales a través de su música. A lo largo del álbum, Marea combina temas personales y políticos, creando un diálogo continuo entre el individuo y la sociedad.\n'La Jauría de Perros Verdes' es aclamado por revitalizar el estilo musical de la banda y por mantener a Marea relevante en la música rock, demostrando que siguen siendo una voz vital y crítica en la escena musical española.",
+    songs: [
+      "Paloma que pierde el vuelo",
+      "En un mercedes blanco",
+      "Cada noche",
+      "La sangre, los polvos, los muertos",
+      "Eutsi goiari",
+      "Chica de la ciudad",
+      "La vela se apaga",
+      "Crazy like a fox",
+      "Señora",
+      "El Luis",
+      "Carta a los corintios según Sor Kampana",
+      "Marea – Versión acústica",
+      "Un cuarto sin ventanas",
+      "Desencuentro",
+      "Malos despertares",
+    ],
+  },
+  {
+    id: 7,
+    title: "Las Putas Más Viejas del Mundo",
+    image: viejasImage,
+    shortDescription:
+      "Un título provocador para un álbum que desafía los límites del rock en español.",
+    longDescription:
+      "Lanzado en 2014, 'Las Putas Más Viejas del Mundo' destaca por su enfoque audaz tanto en la música como en las letras, consolidando a Marea como uno de los actos más provocativos y comprometidos del rock español.\nEste álbum incluye algunos de los trabajos más experimentales de la banda, explorando nuevos sonidos y estructuras musicales. Canciones como 'El Hijo de la Inés' no solo desafían las expectativas sino que también proporcionan una crítica mordaz a la sociedad contemporánea.\nCon 'Las Putas Más Viejas del Mundo', Marea demuestra una vez más su habilidad para combinar poesía profunda con música potente, creando un álbum que no solo entretiene sino que también provoca reflexión y debate, manteniendo su relevancia cultural y artística en el rock.",
+    songs: [
+      "Entre hormigones",
+      "Petenera (en carne viva)",
+      "Con la camisa rota",
+      "Duerme conmigo",
+      "Mierda y cuchara",
+      "Manuela canta saetas",
+      "Que se joda el viento",
+      "Romance de José Etxailarena",
+      "Corazón de mimbre",
+      "Mil quilates",
+    ],
+  },
+  {
+    id: 8,
+    title: "En Mi Hambre Mando Yo",
+    image: hambreImage,
+    shortDescription:
+      "Un álbum que refleja una madurez musical con fuertes raíces en el rock español.",
+    longDescription:
+      "Publicado en 2011, 'En Mi Hambre Mando Yo' marca un momento de consolidación para Marea, donde la banda refina aún más su sonido y su mensaje. Este álbum es notable por su profundidad lírica y su complejidad musical, que captura una variedad de emociones y temas que resonan intensamente con su audiencia.\nDesde temas de amor y pérdida hasta críticas sociales agudas, este álbum no se abstiene de explorar la gama completa de experiencias humanas. Canciones como 'La Rueca' y 'Ojalá Me Quieras Libre' son ejemplos destacados de cómo Marea equilibra la poesía y el rock con maestría.\n'En Mi Hambre Mando Yo' es celebrado por su enfoque introspectivo y emotivo, mostrando cómo Marea ha evolucionado no solo como músicos, sino como narradores, ofreciendo un álbum que es tanto personal como universalmente accesible.",
+    songs: [
+      "Bienvenido al secadero",
+      "La majada",
+      "Sobran bueyes",
+      "El día que lluevan pianos",
+      "Canaleros",
+      "Ojalá me quieras libre",
+      "Ángeles del suelo",
+      "Las últimas habitaciones",
+      "Plomo en los bolsillos",
+      "Pedimento",
+    ],
+  },
+  {
+    id: 9,
+    title: "El Azogue",
+    image: azogueImage,
+    shortDescription:
+      "El último trabajo de Marea, un reflejo de su madurez y maestría musical.",
+    longDescription:
+      "Lanzado en 2019, 'El Azogue' se presenta como el más reciente álbum de Marea, y es un testimonio de la madurez alcanzada por la banda después de décadas en la industria. Este álbum combina la energía característica del rock de Marea con una madurez lírica que profundiza en temas de introspección, política y sociedad.\nCanciones como 'Pájaros en la Cabeza' y 'Jindama' muestran un sonido renovado que sigue siendo fiel a las raíces de la banda. 'El Azogue' es un diálogo entre el pasado y el presente de Marea, explorando nuevos territorios musicales mientras rinde homenaje a su legado.\nEste álbum ha sido aclamado tanto por la crítica como por los fans por su capacidad de reinventarse sin perder la esencia que los ha definido. Con 'El Azogue', Marea no solo celebra su trayectoria, sino que también establece el camino para su futuro musical.",
+    songs: [
+      "En las encías",
+      "Un hierro sin domar",
+      "Muchas lanzas",
+      "Jindama",
+      "La noche de Viernes Santo",
+      "Ocho mares",
+      "Copla del precipicio",
+      "El temblor",
+      "Pájaros viejos",
+      "Pecadores",
+    ],
+  },
+  {
+    id: 10,
+    title: "Los Potros del Tiempo",
+    image: potrosImage,
+    shortDescription:
+      "Exploración profunda de la madurez musical y lírica de Marea.",
+    longDescription:
+      "‘Los Potros del Tiempo’, lanzado en 2022, es la más reciente adición a la discografía de Marea, demostrando una vez más su capacidad para fusionar poesía lírica profunda con rock puro. Este álbum refleja una etapa de reflexión y madurez, llevando a los oyentes en un viaje a través de paisajes sonoros ricos y letras que contemplan tanto el paso del tiempo como los desafíos contemporáneos.\nEl álbum se caracteriza por su tono introspectivo y contemplativo, sin perder la energía característica que ha definido a la banda durante más de dos décadas. Con temas que abordan desde la resiliencia frente a la adversidad hasta celebraciones de la vida cotidiana, 'Los Potros del Tiempo' captura la esencia de Marea al equilibrar el rock duro con baladas emotivas.\nDestacan canciones como ‘Voces que gritan en silencio’, que combina ritmos intensos con letras que resonarán con aquellos que han seguido a la banda a lo largo de los años. Este álbum no solo solidifica el legado de Marea, sino que también asegura su relevancia continua en el panorama musical, prometiendo ser un hito en su carrera.",
+    songs: [
+      "Otra cicatriz",
+      "Buena muerte. Se acaba el baile",
+      "Nuestra fosa",
+      "Más me duele a mí",
+      "Lo habido",
+      "Esta puta soledad",
+      "Ceniciento,La grillera",
+      "El más sucio de los nombres",
+      "Te voy a decir la verdad",
+    ],
+  },
+];
