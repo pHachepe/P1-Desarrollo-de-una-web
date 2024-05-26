b656e9d551b73607a9d9ad2c09663c041dc151fd
PeHachePe <pehachepe@uoc.edu>
2024-05-26 21:21:35 +0200

    upd icons to span

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
   mobileMenu.addEventListener("click", () => {
     menuList.classList.toggle("open");
     mobileMenu.innerHTML = menuList.classList.contains("open")
-      ? '<i class="fas fa-times"></i>'
-      : '<i class="fas fa-bars"></i>';
+      ? '<span class="fas fa-times"></span>'
+      : '<span class="fas fa-bars"></span>';
   });
 
   // Se redirige desde aquí a la página principal porque si se engloba la imagen en una etiqueta <a> se estropea el aspect-ratio de la imagen
