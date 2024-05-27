8fdb3604e910878a757562decb607df3d64ee5b2
PeHachePe <pehachepe@uoc.edu>
2024-05-27 17:13:30 +0200

    Add responsive sizes imgs

--- src/scripts/category.js
+++ src/scripts/category.js
@@ -6,7 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
-      <img loading="lazy" src="${album.image}" alt="${album.title}" />
+      <picture>
+        <source media="(max-width: 167px)" srcset="${album.images.small}">
+        <source media="(min-width: 168px) and (max-width: 334px)" srcset="${album.images.medium}">
+        <source media="(min-width: 335px)" srcset="${album.images.large}">
+        <img loading="lazy" src="${album.images.large}" alt="${album.title}" />
+      </picture>
       <div class="card-body">
         <h3>${album.title}</h3>
         <p>${album.shortDescription}</p>
